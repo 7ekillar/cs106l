f9fa6c638c416df8697711576eb16ddef72e1235
7ekillar <2488491236@qq.com>
2026-03-14 17:10:54 +0800

    assignment1 翻译     对assignment1的README.md和utils.cpp文件进行翻译

--- assignment1/utils.cpp
+++ assignment1/utils.cpp
@@ -6,10 +6,21 @@
  * implementing the assignment. You don't need to modify anything here,
  * but feel free to look around.
  */
+/*
+ * CS106L 作业 1: SimpleEnroll
+ * 由 Fabio Ibanez 创建，Jacob Roberts-Baca 修改。
+ *
+ * 本文件包含一些在完成作业时可能有用的实用函数。
+ * 你不需要修改这里的任何内容，但可以随意查看。
+ */
 
 /**
  * Forward declarations of some important methods. Don't worry about these!
  * (unless you really want to).
+ */
+/**
+ * 一些重要方法的前向声明。不必担心这些！
+ * （除非你真的很想知道）。
  */
 bool operator==(const Course& lhs, const Course& rhs);
 std::ostream& operator<<(std::ostream& os, const Course& course);
@@ -19,6 +30,12 @@ std::ostream& operator<<(std::ostream& os, const Course& course);
  * @param s     The string to split.
  * @param delim The delimiter to split by.
  * @returns     A vector of the split strings.
+ */
+/**
+ * 使用分隔符分割字符串，并返回分割后的字符串向量。
+ * @param s     要分割的字符串。
+ * @param delim 用于分割的分隔符。
+ * @returns     分割后的字符串向量。
  */
 std::vector<std::string> split(const std::string& s, char delim) {
   std::vector<std::string> return_vec;
@@ -37,6 +54,13 @@ std::vector<std::string> split(const std::string& s, char delim) {
  *
  * We will learn more about what's going on in this function in Weeks 3 and 5!
  */
+/**
+ * 从课程向量中删除一个 Course 对象。
+ * @param v    课程向量。
+ * @param elem 要删除的课程。
+ *
+ * 我们将在第 3 周和第 5 周更深入地了解这个函数内部的工作原理！
+ */
 void delete_elem_from_vector(std::vector<Course>& v, const Course& elem) {
   std::vector<Course>::iterator it = std::find(v.begin(), v.end(), elem);
   v.erase(it);
@@ -45,6 +69,10 @@ void delete_elem_from_vector(std::vector<Course>& v, const Course& elem) {
 /**
  * Prints a vector of courses (for debugging purposes)!
  * @param vector_of_courses The vector of courses to print.
+ */
+/**
+ * 打印课程向量（用于调试目的）！
+ * @param vector_of_courses 要打印的课程向量。
  */
 void print_courses(const std::vector<Course>& vector_of_courses) {
   for (const Course& course : vector_of_courses) {
@@ -55,6 +83,10 @@ void print_courses(const std::vector<Course>& vector_of_courses) {
 /* ========================================================================= *
  * Don't worry about anything beyond this point.                             *
  * (unless you are really curious and want to!)                              *
+ * ========================================================================= */
+/* ========================================================================= *
+ * 不要担心此点之后的内容。                                                   *
+ * （除非你真的很感兴趣并且想要了解！）                                        *
  * ========================================================================= */
 
 bool operator==(const Course& lhs, const Course& rhs) {
