0608c6a165277b148aff4fe8a07eecabf77f15d2
7ekillar <2488491236@qq.com>
2026-03-19 23:02:22 +0800

    assignment2 翻译     对assignment2的README.md文件和main.cpp文件翻译。

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -6,6 +6,14 @@
  * in this file. You do not need to modify any other files.
  *
  */
+/*
+ * CS106L 作业 2：婚姻协议
+ * 由 Haven Whitney 创建，Fabio Ibanez 和 Jacob Roberts-Baca 修改。
+ *
+ * 欢迎来到 CS106L 的作业 2！请完成本文件中的每个“学生待办”事项。
+ * 你不需要修改任何其他文件。
+ *
+ */
 
 #include <fstream>
 #include <iostream>
@@ -15,6 +23,7 @@
 #include <unordered_set>
 
 std::string kYourName = "STUDENT TODO"; // Don't forget to change this!
+// 别忘了修改这个！
 
 /**
  * Takes in a file name and returns a set containing all of the applicant names as a set.
@@ -27,8 +36,19 @@ std::string kYourName = "STUDENT TODO"; // Don't forget to change this!
  * below it) to use a `std::unordered_set` instead. If you do so, make sure
  * to also change the corresponding functions in `utils.h`.
  */
+/**
+ * 接收文件名，返回包含所有申请者姓名的集合。
+ *
+ * @param filename  要读取的文件名。
+ *                  文件的每一行都是一个申请者的姓名。
+ * @returns         从文件中读取的所有申请者姓名的集合。
+ *
+ * @remark 可以随意更改此函数（以及下面的函数）的返回类型，改用 `std::unordered_set`。
+ *         如果这样做，请确保同时修改 `utils.h` 中相应的函数。
+ */
 std::set<std::string> get_applicants(std::string filename) {
   // STUDENT TODO: Implement this function.
+  // 学生待办：实现此函数。
 }
 
 /**
@@ -39,8 +59,16 @@ std::set<std::string> get_applicants(std::string filename) {
  * @param students  The set of student names.
  * @return          A queue containing pointers to each matching name.
  */
+/**
+ * 接收学生姓名集合的引用，并返回一个与给定学生姓名匹配的姓名队列。
+ *
+ * @param name      返回的姓名队列应与此姓名的首字母相同。
+ * @param students  学生姓名集合。
+ * @return          包含每个匹配姓名指针的队列。
+ */
 std::queue<const std::string*> find_matches(std::string name, std::set<std::string>& students) {
   // STUDENT TODO: Implement this function.
+   // 学生待办：实现此函数
 }
 
 /**
@@ -53,9 +81,20 @@ std::queue<const std::string*> find_matches(std::string name, std::set<std::stri
  * @return        Your magical one true love.
  *                Will return "NO MATCHES FOUND." if `matches` is empty.
  */
+/**
+ * 接收指向可能匹配对象的指针队列，并确定唯一真命天子/天女！
+ *
+ * 你可以按自己喜欢的方式实现此函数，但尽量做一些比简单的 `pop()` 更复杂一点的操作。
+ *
+ * @param matches 可能匹配对象的队列。
+ * @return        你神奇的唯一真爱。
+ *                如果 `matches` 为空，则返回 "NO MATCHES FOUND."。
+ */
 std::string get_match(std::queue<const std::string*>& matches) {
   // STUDENT TODO: Implement this function.
+  // 学生待办：实现此函数。
 }
 
 /* #### Please don't remove this line! #### */
+/* #### 请不要删除此行！ #### */
 #include "autograder/utils.hpp"
